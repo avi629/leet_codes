61851204a33ad042b45dd85a5c45abcb4de1a88c
Abhisek sahu <infonet529@gmail.com>
2026-07-03 08:20:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (14.76%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findDelayedArrivalTime(self, arrivalTime, delayedTime):
+        """
+        :type arrivalTime: int
+        :type delayedTime: int
+        :rtype: int
+        """
+        return (arrivalTime + delayedTime) % 24
+        

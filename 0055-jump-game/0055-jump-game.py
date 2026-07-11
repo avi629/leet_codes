3ddd56b26191ebcf82af0b4085eb61cb267e2e87
Abhisek sahu <infonet529@gmail.com>
2026-07-11 08:45:54 +0530

    Time: 59 ms (6.26%), Space: 13.3 MB (61.78%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxReach = 0
+        for i in range(len(nums)):
+            if maxReach < i:
+                return False
+
+            maxReach = max(maxReach , i + nums[i])
+        return True

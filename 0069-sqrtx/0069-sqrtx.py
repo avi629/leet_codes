628a32303b77a190c48e34eb9e8084074be6430e
Abhisek sahu <infonet529@gmail.com>
2026-07-14 21:40:06 +0530

    Time: 8 ms (20.6%), Space: 12.3 MB (89.67%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # if x < 2:
+        #     return x
+
+        # left = 1
+        # right = x
+        # while left <= right :
+        #     mid = (left + right) //2
+        #     if mid*mid == x:
+        #         return mid
+        #     if mid*mid < x:
+        #         left = mid +1
+        #     if mid*mid > x:
+        #         right = mid -1
+
+        # return right        # integer square root - binary search
+
+################################################
+        if x < 2:
+            return x
+
+        left = 1 
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+            if mid * mid < x:
+                left = mid + 1
+            if mid * mid > x:
+                right = mid -1
+
+        return right
+
+        

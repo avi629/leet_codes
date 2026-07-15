6b17aa2dbce8ebb86632d74947bf8e4b3b81651e
Abhisek sahu <infonet529@gmail.com>
2026-07-15 21:16:02 +0530

    Time: 36 ms (20.81%), Space: 12.3 MB (61.75%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        return n
+        total1 = 0
+        for i in range(n):
+            total1 += 2*i + 1  # sum of first n odd numbers
+
+        total2 = 0
+        for j in range(n):
+            total2 += 2*j + 2  # sum of first n even numbers
+        
+        a = total1
+        b = total2
+
+        while b != 0:           # to get GCD
+            a , b = b , a%b
+        return a

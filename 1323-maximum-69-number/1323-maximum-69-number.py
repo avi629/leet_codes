1556ed1a5d865db15b11720bf143ed0102d1dedc
Abhisek sahu <infonet529@gmail.com>
2026-08-01 20:11:34 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.18%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maximum69Number (self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        digits = list(str(num))
+        for i in range(len(digits)):
+            if digits[i] == '6':
+                digits[i] = '9'
+                break
+        result = int(''.join(digits))
+        return result
+
+######################################################
+        original = num
+        place = 1
+        six_place = 0
+        while num != 0:
+            digit = num % 10
+            if digit == 6:
+                six_place = place
+            place *= 10
+            num = num // 10
+        final_answer = original + (3 * six_place)
+        return final_answer

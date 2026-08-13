ceb1eabb7e816ad25d6acea1a2b70e9a2bf03a63
Abhisek Sahu <infonet529@gmail.com>
2026-08-13 09:37:34 +0530

    Time: 4 ms (32.76%), Space: 12.3 MB (89.3%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # seen = set()
+
+        # while n != 1:
+        #     if n in seen:
+        #         return False
+
+        #     seen.add(n)
+
+        #     total = 0
+        #     while n > 0 :
+        #         digit = n % 10
+        #         total = total + digit * digit
+        #         n = n // 10
+        
+        #     n = total
+
+        # return True
+        
+
+########################################################
+        seen = set()
+        
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+
+            total = 0
+            while n > 0:
+                digit = n % 10
+                total = total + digit * digit
+                n = n// 10
+
+            n = total
+
+        return True

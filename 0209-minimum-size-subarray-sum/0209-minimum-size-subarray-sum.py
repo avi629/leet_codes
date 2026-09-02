c206896c930f8377a08c8711b197eb8a5978031a
Abhisek Sahu <infonet529@gmail.com>
2026-09-02 20:04:29 +0530

    Time: 27 ms (44.89%), Space: 20 MB (46.05%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,6 +5,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # left = 0
+        # curr_sum = 0
+        # min_len = float('inf')
+
+        # for right in range(len(nums)):
+        #     curr_sum += nums[right]
+
+        #     while curr_sum >= target:
+        #         min_len = min(right - left +1 , min_len)
+
+        #         curr_sum -= nums[left]
+        #         left += 1
+
+        # if min_len == float('inf'):
+        #     return 0
+
+        # return min_len
+
+
+#################################################################
         left = 0
         curr_sum = 0
         min_len = float('inf')
@@ -13,7 +33,7 @@ class Solution(object):
             curr_sum += nums[right]
 
             while curr_sum >= target:
-                min_len = min(right - left +1 , min_len)
+                min_len = min(right-left+1 , min_len)
 
                 curr_sum -= nums[left]
                 left += 1
@@ -22,4 +42,3 @@ class Solution(object):
             return 0
 
         return min_len
-

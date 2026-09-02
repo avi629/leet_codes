37cf7bef36174596e3d5d6e62c24faf2f5cb9911
Abhisek Sahu <infonet529@gmail.com>
2026-09-02 08:21:18 +0530

    Time: 0 ms (100%), Space: 13.9 MB (43.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,17 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        left = 1
+        for right in range(1,len(nums)):
+            if nums[right] != nums[right-1]:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
+
+
+
+
         # i = 0 
         # for j in range(1 , len(nums)):
         #     if nums[j] != nums[i]:
         #         i = i+1 # it use to move the pointer to the nextposn 
         #         nums[i] = nums[j] #It copies a value from j to i , it runs when the numbers are different
         # return i+1
+
+############################################################ 
+
+        # k = 1
+        # for i in range(1 , len(nums)):
+        #     if nums[i] != nums[i-1]:
+        #         nums[k] = nums[i]
+        #         k += 1
         
-        k = 1
-        for i in range(1 , len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[k] = nums[i]
-                k += 1
-        
-        return k
+        # return k

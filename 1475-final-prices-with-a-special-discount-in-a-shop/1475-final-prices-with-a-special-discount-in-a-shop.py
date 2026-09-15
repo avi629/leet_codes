4615e06ca481281a4e8d3486669ecfd063b9ac08
Abhisek Sahu <infonet529@gmail.com>
2026-09-15 08:12:33 +0530

    Time: 2 ms (81.3%), Space: 12.5 MB (13.98%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -4,15 +4,33 @@ class Solution(object):
         :type prices: List[int]
         :rtype: List[int]
         """
-        result = []
-        for i in range(len(prices)):
-            for j in range(i+1 , len(prices)):
-                if prices[j] <= prices[i]:
-                    result.append(prices[i]-prices[j])
-                    break
-            else:
-                result.append(prices[i])
-        return result
+        stack = []
+
+        for i in range(len(prices)-1 , -1 , -1):
+            price = prices[i]
+
+            while stack and stack[-1] > price:
+                stack.pop()
+            
+            if stack:
+                prices[i] = price - stack[-1]
+            
+            stack.append(price)
+
+        return prices
+
+
+
+#################################################
+        # result = []
+        # for i in range(len(prices)):
+        #     for j in range(i+1 , len(prices)):
+        #         if prices[j] <= prices[i]:
+        #             result.append(prices[i]-prices[j])
+        #             break
+        #     else:
+        #         result.append(prices[i])
+        # return result
         
 
 

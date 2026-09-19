04edbc7f8b282ac4b9762d00464bb86d737b54c3
Abhisek Sahu <infonet529@gmail.com>
2026-09-19 08:22:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (25.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -7,21 +7,41 @@ class Solution(object):
         """
         stack = []
         next_greater = {}
+
         for num in nums2:
             while stack and num > stack[-1]:
                 next_greater[stack.pop()] = num
-            
+        
             stack.append(num)
         
-        while stack :
-            next_greater[stack.pop()] = -1
+        while stack:
+            next_greater[stack.pop()]= -1
         
         ans = []
-
         for num in nums1:
             ans.append(next_greater[num])
         
         return ans
+
+
+######################################################################
+        # stack = []
+        # next_greater = {}
+        # for num in nums2:
+        #     while stack and num > stack[-1]:
+        #         next_greater[stack.pop()] = num
+            
+        #     stack.append(num)
+        
+        # while stack :
+        #     next_greater[stack.pop()] = -1
+        
+        # ans = []
+
+        # for num in nums1:
+        #     ans.append(next_greater[num])
+        
+        # return ans
 
 
 ###############################################################

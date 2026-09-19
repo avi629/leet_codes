f77877c22c05b7d78012ba718ad03c0cad44dbe7
Abhisek Sahu <infonet529@gmail.com>
2026-09-19 15:52:07 +0530

    Time: 71 ms (50%), Space: 16.5 MB (53.33%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -16,8 +16,26 @@ class Solution(object):
                 right -= 1
             else:
                 right -= 1
-
+            
             boats += 1
 
         return boats
+
+
+   ###################################################     
+        # people.sort()
+        # left = 0
+        # right = len(people) - 1
+        # boats = 0
+
+        # while left <= right:
+        #     if people[left] + people[right] <= limit:
+        #         left += 1
+        #         right -= 1
+        #     else:
+        #         right -= 1
+
+        #     boats += 1
+
+        # return boats
 

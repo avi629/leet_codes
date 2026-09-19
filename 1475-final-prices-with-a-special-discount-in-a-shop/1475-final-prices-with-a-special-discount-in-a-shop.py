25846c327feccf9846445e6feba3c57d66b44008
Abhisek Sahu <infonet529@gmail.com>
2026-09-19 08:46:33 +0530

    Time: 3 ms (76.71%), Space: 12.6 MB (13.5%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -5,19 +5,34 @@ class Solution(object):
         :rtype: List[int]
         """
         stack = []
-
-        for i in range(len(prices)-1 , -1 , -1):
+        for i in range(len(prices)-1, -1  ,-1):
             price = prices[i]
 
             while stack and stack[-1] > price:
                 stack.pop()
-            
+
             if stack:
                 prices[i] = price - stack[-1]
             
             stack.append(price)
 
         return prices
+
+#################################################
+        # stack = []
+
+        # for i in range(len(prices)-1 , -1 , -1):
+        #     price = prices[i]
+
+        #     while stack and stack[-1] > price:
+        #         stack.pop()
+            
+        #     if stack:
+        #         prices[i] = price - stack[-1]
+            
+        #     stack.append(price)
+
+        # return prices
 
 
 
